84f8398e9e0b950a556aa5d7f06cd1d40c41ceca
Hriidhaan <164275844+Hriidhaan@users.noreply.github.com>
2024-03-20 19:45:57 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,30,30);
+}
+
+function draw() 
+{
+  background(30);
+  if (keyDown(RIGHT_ARROW)) {
+    box.x+=10
+  }
+  if (keyDown(LEFT_ARROW)) {
+    box.x-=10
+  }
+  if (keyDown(UP_ARROW)) {
+    box.y-=10
+  }
+  if (keyDown(DOWN_ARROW)) {
+    box.y+=10
+  }
+  drawSprites();
+}
+
+
+
+
